2cc8495f2d4e65f30cfbcb73f5a81d0ffb0fbfa9
ChinaNuke <chinanuke@nuke666.cn>
2021-11-29 09:34:52 +0800

    格式化代码，添加编译bc文件的脚本

--- LLVMAssignment.cpp
+++ LLVMAssignment.cpp
@@ -12,19 +12,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <llvm/Support/CommandLine.h>
-#include <llvm/IRReader/IRReader.h>
 #include <llvm/IR/LLVMContext.h>
-#include <llvm/Support/SourceMgr.h>
 #include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/ToolOutputFile.h>
 
 #include <llvm/Bitcode/BitcodeReader.h>
 #include <llvm/Bitcode/BitcodeWriter.h>
 
-
-#include <llvm/Transforms/Utils.h>
 #include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Utils.h>
 
 #include <llvm/IR/Function.h>
 #include <llvm/Pass.h>
@@ -36,75 +35,68 @@ static ManagedStatic<LLVMContext> GlobalContext;
 static LLVMContext &getGlobalContext() { return *GlobalContext; }
 
 struct EnableFunctionOptPass : public FunctionPass {
-    static char ID;
-    EnableFunctionOptPass() :FunctionPass(ID) {}
-    bool runOnFunction(Function & F) override {
-        if (F.hasFnAttribute(Attribute::OptimizeNone))
-        {
-            F.removeFnAttr(Attribute::OptimizeNone);
-        }
-        return true;
+  static char ID;
+  EnableFunctionOptPass() : FunctionPass(ID) {}
+  bool runOnFunction(Function &F) override {
+    if (F.hasFnAttribute(Attribute::OptimizeNone)) {
+      F.removeFnAttr(Attribute::OptimizeNone);
     }
+    return true;
+  }
 };
 
 char EnableFunctionOptPass::ID = 0;
 
 ///!TODO TO BE COMPLETED BY YOU FOR ASSIGNMENT 3
 struct FuncPtrPass : public ModulePass {
-    static char ID; // Pass identification, replacement for typeid
-    FuncPtrPass() : ModulePass(ID) {}
+  static char ID; // Pass identification, replacement for typeid
+  FuncPtrPass() : ModulePass(ID) {}
 
-
-    bool runOnModule(Module &M) override {
-        errs() << "Hello: ";
-        errs().write_escaped(M.getName()) << '\n';
-        M.dump();
-        errs() << "------------------------------\n";
-        return false;
-    }
+  bool runOnModule(Module &M) override {
+    errs() << "Hello: ";
+    errs().write_escaped(M.getName()) << '\n';
+    M.dump();
+    errs() << "------------------------------\n";
+    return false;
+  }
 };
 
-
 char FuncPtrPass::ID = 0;
-static RegisterPass<FuncPtrPass> X("funcptrpass", "Print function call instruction");
+static RegisterPass<FuncPtrPass> X("funcptrpass",
+                                   "Print function call instruction");
 
 char Liveness::ID = 0;
 static RegisterPass<Liveness> Y("liveness", "Liveness Dataflow Analysis");
 
 static cl::opt<std::string>
-InputFilename(cl::Positional,
-              cl::desc("<filename>.bc"),
-              cl::init(""));
-
+    InputFilename(cl::Positional, cl::desc("<filename>.bc"), cl::init(""));
 
 int main(int argc, char **argv) {
-   LLVMContext &Context = getGlobalContext();
-   SMDiagnostic Err;
-   // Parse the command line to read the Inputfilename
-   cl::ParseCommandLineOptions(argc, argv,
-                              "FuncPtrPass \n My first LLVM too which does not do much.\n");
+  LLVMContext &Context = getGlobalContext();
+  SMDiagnostic Err;
+  // Parse the command line to read the Inputfilename
+  cl::ParseCommandLineOptions(
+      argc, argv, "FuncPtrPass \n My first LLVM too which does not do much.\n");
 
+  // Load the input module
+  std::unique_ptr<Module> M = parseIRFile(InputFilename, Err, Context);
+  if (!M) {
+    Err.print(argv[0], errs());
+    return 1;
+  }
 
-   // Load the input module
-   std::unique_ptr<Module> M = parseIRFile(InputFilename, Err, Context);
-   if (!M) {
-      Err.print(argv[0], errs());
-      return 1;
-   }
-
-   llvm::legacy::PassManager Passes;
+  llvm::legacy::PassManager Passes;
 #if LLVM_VERSION_MAJOR == 5
-   Passes.add(new EnableFunctionOptPass());
+  Passes.add(new EnableFunctionOptPass());
 #endif
-   ///Transform it to SSA
-   Passes.add(llvm::createPromoteMemoryToRegisterPass());
+  /// Transform it to SSA
+  Passes.add(llvm::createPromoteMemoryToRegisterPass());
 
-   /// Your pass to print Function and Call Instructions
-   Passes.add(new Liveness());
-   //Passes.add(new FuncPtrPass());
-   Passes.run(*M.get());
+  /// Your pass to print Function and Call Instructions
+  Passes.add(new Liveness());
+  // Passes.add(new FuncPtrPass());
+  Passes.run(*M.get());
 #ifndef NDEBUG
-   system("pause");
+  system("pause");
 #endif
 }
-
